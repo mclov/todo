8af8ae268f9b2574e0035cb6f4cea8070b39d289
Michael Clovis <michaelclovis@protonmail.com>
2019-12-08 15:29:32 -0800

    create TodoList and Todo

---
+++ src/TodoList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+export class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{ task: 'Code a todo app' }]
+    }
+  }
+
+  render() {
+    const todos = this.state.todos.map(todo => {
+      return <Todo task={todo.task} />
+    })
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ul>
+          {todos}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TodoList
