9ed1378f51ca8ea449e0c9491f8c9fe6c6480ac4
Michael Clovis <michaelclovis@protonmail.com>
2019-12-08 20:44:12 -0800

    add styling

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
-import uuid from 'uuid/v4';
+import React, { Component } from "react";
+import uuid from "uuid/v4";
+import "./NewTodoForm.css";
 
-
-export class NewTodoForm extends Component {
+class NewTodoForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      task: ''
-    }
+    this.state = { task: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
-    })
+    });
   }
-
   handleSubmit(evt) {
     evt.preventDefault();
     this.props.createTodo({ ...this.state, id: uuid(), completed: false });
-    this.setState({
-      task: ''
-    })
+    this.setState({ task: "" });
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form className='NewTodoForm' onSubmit={this.handleSubmit}>
         <label htmlFor='task'>New Todo</label>
         <input
           type='text'
@@ -39,8 +33,7 @@ export class NewTodoForm extends Component {
         />
         <button>Add Todo</button>
       </form>
-    )
+    );
   }
 }
-
-export default NewTodoForm
+export default NewTodoForm;
--- src/Todo.js
+++ src/Todo.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
-import './Todo.css';
+import React, { Component } from "react";
+import "./Todo.css";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-export class Todo extends Component {
+class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isEditing: false,
       task: this.props.task
-    }
+    };
     this.handleRemove = this.handleRemove.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleUpdate = this.handleUpdate.bind(this);
     this.handleToggle = this.handleToggle.bind(this);
   }
-
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
-
   toggleForm() {
-    this.setState({
-      isEditing: !this.state.isEditing
-    })
+    this.setState({ isEditing: !this.state.isEditing });
   }
-
   handleUpdate(evt) {
     evt.preventDefault();
+    //take new task data and pass up to parent
     this.props.updateTodo(this.props.id, this.state.task);
-    this.toggleForm()
+    this.setState({ isEditing: false });
   }
-
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
-    })
+    });
   }
-
   handleToggle(evt) {
-    this.props.toggleTodo(this.props.id)
+    this.props.toggleTodo(this.props.id);
   }
-
   render() {
     let result;
     if (this.state.isEditing) {
       result = (
-        <div>
-          <form onSubmit={this.handleUpdate}>
+        <CSSTransition key='editing' timeout={500} classNames='form'>
+          <form className='Todo-edit-form' onSubmit={this.handleUpdate}>
             <input
               type='text'
               value={this.state.task}
@@ -55,20 +49,32 @@ export class Todo extends Component {
             />
             <button>Save</button>
           </form>
-        </div>
-      )
+        </CSSTransition>
+      );
     } else {
       result = (
-        <div>
-          <button onClick={this.toggleForm}>Edit</button>
-          <button onClick={this.handleRemove}>X</button>
-          <li className={this.props.completed && 'completed'} onClick={this.handleToggle}>{this.props.task}</li>
-        </div>
-      )
+        <CSSTransition key='normal' timeout={500} classNames='task-text'>
+          <li className='Todo-task' onClick={this.handleToggle}>
+            {this.props.task}
+          </li>
+        </CSSTransition>
+      );
     }
-
-    return result
+    return (
+      <TransitionGroup
+        className={this.props.completed ? "Todo completed" : "Todo"}
+      >
+        {result}
+        <div className='Todo-buttons'>
+          <button onClick={this.toggleForm}>
+            <i class='fas fa-pen' />
+          </button>
+          <button onClick={this.handleRemove}>
+            <i class='fas fa-trash' />
+          </button>
+        </div>
+      </TransitionGroup>
+    );
   }
 }
-
-export default Todo
+export default Todo;
