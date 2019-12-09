51e7542e0f9bba917d0f08cb29d8b9d176dc111b
Michael Clovis <michaelclovis@protonmail.com>
2019-12-08 21:09:30 -0800

    app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoList from './TodoList';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoList />
+      </div >
+    );
+  }
 }
 
 export default App;
